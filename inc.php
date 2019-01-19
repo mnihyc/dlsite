90fb8df5eea7283c7d5ab88cd7a51c3494a9eb56
mnihyc <mnihyc@qq.com>
2019-01-19 14:09:59 +0800

    Rebuild the project

---
+++ inc.php
@@ -0,0 +1,310 @@
+<?php
+    /* Set envirenment to chinese */
+    setlocale(LC_ALL,'zh_CN.UTF8');
+
+    /* Directory of these two .php files(/ as webroot) */
+    define(ROOT_DIR,'');
+    
+    /* Directory of the main folder(/ as webroot) */
+    define(FILE_DIR,'/dl');
+    
+    /* The token of password verification */
+    define(DEF_PASS,'123456');
+    
+    /* The token of downloading files */
+    define(DEF_DOWN,'"7891011');
+    
+    /* Buffer size of downloading */
+    define(READ_BS,1024*1024);
+    
+    /* Password file of a directory */
+    define(DIRPASS_NAME,'/.pass');
+    
+    
+    if(IS_IN_PHP!==true)
+        diemsg('Great job, you found one of my configuration files!!!<br /> (But actually that\'s useless.(wwwwww2333333');
+
+    /* Execute scripts or show html pages */
+    function commandefiles($path)
+    {
+        if(substr($path,-4)==='.php')
+        {
+            require $path;
+            die();
+        }
+        if(substr($path,-5)==='.html' || substr($path,-3)==='.js')
+        {
+            echo file_get_contents($path);
+            die();
+        }
+    }
+
+    /* Filter a path */
+    function filterpath(&$path)
+    {
+        $count=1;
+        while($count)
+            $path=str_replace('//','/',$path,$count);
+    }
+
+    /* urlencode() a directory */
+    function encodedir($path)
+    {
+        $str='';
+        $arr=explode('/',$path);
+        unset($arr[0]);
+        foreach($arr as $key => $val)
+            $str.=('/'.urlencode($val));
+        return $str;
+    }
+    
+    /* Check for the verification */
+    function checkpassword(&$inpassver,&$inpasswd,$passwd,$opath)
+    {
+?>
+<div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="d-table-cell">
+                        <div class="container">
+                            <p class="lead text-center">This file/directory is protected with <strong>password</strong>. <br></p>
+<?php
+        if($inpassver)
+        {
+            if(substr($inpasswd,0,5)==='pass=')
+            {
+                $inpasswd=substr($inpasswd,5);
+                ob_end_clean();
+                header('Location: '.encodedir(ROOT_DIR.$opath).'?'.urlencode(encrypt(strval(time()).'|'.$inpasswd.'|'.$opath,'E',DEF_PASS)),TRUE,301);
+                die();
+            }
+            $inpasswd=encrypt($inpasswd,'D',DEF_PASS);
+            if($inpasswd!==FALSE && strpos($inpasswd,'|')!==FALSE)
+            {
+                $arr=explode('|',$inpasswd);
+                $passtime=intval($arr[0]);
+                if(abs(time()-$passtime)>=3600)
+                {
+                    $inpassver=true;
+                    echo '<p class="lead text-center">Verification <span style="color: red;"><strong>expired</strong></span>.</p>';
+                }
+                else
+                {
+                    $inpasswd=$arr[1];
+                    if($inpasswd!==$passwd || $arr[2]!==$opath)
+                    {
+                        $inpassver=true;
+                        echo '<p class="lead text-center">Verification <span style="color: red;"><strong>failed</strong></span>.</p>';
+                    }
+                    else
+                    {
+                        $inpassver=false;
+                        echo '<p class="lead text-center">Verification <span style="color: green;"><strong>passed</strong></span>.</p>';
+                    }
+                }
+            }
+            else
+            {
+                $inpassver=true;
+                echo '<p class="lead text-center">Verification <span style="color: red;"><strong>failed</strong></span> unexpectedly.</p>';
+            }
+        }
+        else
+            $inpassver=true;
+        
+        if($inpassver)
+        {
+?>
+<form action="<?php echo encodedir(ROOT_DIR.$opath); ?>">
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th style="width:432px;"><input class="form-control d-table ml-auto" type="password" name="pass" autofocus="" autocomplete="off" style="width:441px;"></th>
+                    <th style="width:191px;"><button class="btn btn-dark" type="submit">Verify</button></th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</form>
+<?php
+        }
+?>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+<?php
+    }
+
+    /* Check whether a file is accessible */
+    function invalidfilename($name)
+    {
+        if(substr(basename($name),-5)==='.pass' || substr(basename($name),0,1)==='.')
+            return true;
+        return false;
+    }
+
+    /* Read the password of a file */
+    function getfilepass($opath,$type='filepass')
+    {
+        $passpath=dirname(__FILE__).FILE_DIR.$opath.'.pass';
+        $passver=false;$passwd='';$checkm=true;
+        if(file_exists($passpath))
+        {
+            $passver=true;
+            $passwdarr=parse_ini_file($passpath);
+            $passwd=$passwdarr[$type];
+            if(!isset($passwd))
+            {
+                $passver=false;
+                $checkm=false;
+            }
+            else
+                $passwd=urldecode($passwd);
+        }
+        if(!$passver && $checkm)
+            $passwd=getdirpass(dirname($opath),$type);
+        if(!isset($passwd) || $passwd===FALSE)
+            $passver=false;
+        else
+            $passver=true;
+        return ($passver==false ? FALSE : $passwd);
+    }
+    
+    /* Read the password in all sub-directory */
+    function getdirpass($tpath,$type='dirpass')
+    {
+        $first=false;
+        while(!empty($tpath) && !$first)
+        {
+            if($tpath==='/')
+                $first=true;
+            $tspath=dirname(__FILE__).FILE_DIR.$tpath.DIRPASS_NAME;
+            if(file_exists($tspath))
+            {
+                $passver=true;
+                $passwdarr=parse_ini_file($tspath);
+                $passwd=$passwdarr[$type];
+                if(!isset($passwd))
+                    $passver=false;
+                break;
+            }
+            $tpath=dirname($tpath);
+        }
+        return ($passver==false ? FALSE : urldecode($passwd));
+    }
+
+    /* Encrypt the string */
+    function encrypt($string,$operation,$key='')
+    {
+        $key=md5($key);
+        $key_length=strlen($key);
+        $string=$operation=='D'?base64_decode($string):substr(md5($string.$key),0,8).$string;
+        $string_length=strlen($string);
+        $rndkey=$box=array();
+        $result='';
+        for($i=0;$i<=255;$i++)
+        {
+            $rndkey[$i]=ord($key[$i%$key_length]);
+            $box[$i]=$i;
+        }
+        for($j=$i=0;$i<256;$i++)
+        {
+            $j=($j+$box[$i]+$rndkey[$i])%256;
+            $tmp=$box[$i];
+            $box[$i]=$box[$j];
+            $box[$j]=$tmp;
+        }
+        for($a=$j=$i=0;$i<$string_length;$i++)
+        {
+            $a=($a+1)%256;
+            $j=($j+$box[$a])%256;
+            $tmp=$box[$a];
+            $box[$a]=$box[$j];
+            $box[$j]=$tmp;
+            $result.=chr(ord($string[$i])^($box[($box[$a]+$box[$j])%256]));
+        }
+        if($operation=='D')
+            if(substr($result,0,8)==substr(md5(substr($result,8).$key),0,8))
+                return substr($result,8);
+            else
+                return false;
+        else
+            return str_replace('=','',base64_encode($result));
+    }
+
+    /* Get the size of a file */
+    function getfilesize($path)
+    {
+        $size=0.0;
+        $size=filesize($path);
+        
+        $type=' Bytes';
+        if($size>=1024*1024*1024)
+        {
+            $size/=1024*1024*1024;
+            $type=' GB';
+        }
+        else if($size>=1024*1024)
+        {
+            $size/=1024*1024;
+            $type=' MB';
+        }
+        else if($size>=1024)
+        {
+            $size/=1024;
+            $type=' KB';
+        }
+        
+        if(floatval($size)==intval($size))
+            $str=sprintf("%d",$size);
+        else
+            $str=sprintf("%1\$.2f",$size);
+        return $str.$type;
+    }
+
+    /* Output an error message */
+    function diemsg($msg='An unknown error occurs.')
+    {
+        htmlmsg();
+        echo '<h1 class="text-center" style="margin:46px;">'.$msg.'</h1>';
+        echo '<p class="lead text-right" style="padding:25px;margin:0px;">Need support? Please contant the server administrator at mnihyc@qq.com ......</p>';
+        htmlmsg(false);
+        die();
+    }
+    
+    /* Output the html header/footer */
+    function htmlmsg($header=true)
+    {
+        if($header)
+        {header("Pragma: no-cache");?>
+<!DOCTYPE html>
+<!-- Code by mnihyc -->
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo ROOT_DIR ?>/assets/bootstrap/css/bootstrap.min.css">
+        <title>File Download Service</title>
+    </head>
+    
+    <body>
+        <script src="<?php echo ROOT_DIR ?>/assets/js/jquery.min.js"></script>
+        <script src="<?php echo ROOT_DIR ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+    
+        <?php }
+        else
+        {?>
+    
+    </body>
+</html>
+        <?php }
+    }
+?>
---
+++ view.php
@@ -0,0 +1,231 @@
+<?php
+    define(IS_IN_PHP,true);
+    include 'inc.php';
+    
+    /* Get the path as well as password(if exists) */
+    $opath=urldecode($_SERVER['REQUEST_URI']);
+    $opath=str_replace(ROOT_DIR,'',$opath,$cnt);
+    $inpassver=false;$inpasswd='';
+    if(strpos($opath,'?')!==FALSE)
+    {
+        $inpassver=true;
+        $vpos=strpos($opath,'?');
+        $inpasswd=substr($opath,$vpos+1);
+        $opath=substr($opath,0,$vpos);
+    }
+    filterpath($opath);
+    $path=dirname(__FILE__).FILE_DIR.$opath;
+    if(!empty(ROOT_DIR) && $cnt==0)
+        diemsg();
+    if(invalidfilename($path))
+        diemsg('Access denied.');
+    if(strpos($path,'../')!==FALSE || strpos($path,'/..')!==FALSE)
+        diemsg('Invalid filename!');
+    $path=str_replace('./','',$path);
+    
+    /* Different operations for the file and directory */
+    if(is_file($path))
+    {
+        /* Specially command for executable files */
+        commandefiles($path);
+        
+        $passwd=getfilepass($opath);
+        if($passwd===FALSE)
+            $passver=false;
+        else
+            $passver=true;
+        
+        /* No password verification and a direct link requirement */
+        if(!$passver && $inpasswd==='direct')
+        {
+            header('Location: '.encodedir(ROOT_DIR.$opath).'?'.urlencode(encrypt('on'.'|'.($passver ? $inpasswd : '').'|'.strval(time()).'|'.$opath,'E',DEF_DOWN)),TRUE,301);
+            die();
+        }
+        
+        /* This is a downloading link */
+        $downs=encrypt($inpasswd,'D',DEF_DOWN);
+        if($downs!==FALSE && strpos($downs,'|')!==FALSE)
+        {
+            $arr=explode('|',$downs);
+            if($arr[0]!=='dn' && $arr[0]!=='on')
+                diemsg();
+            $passtime=intval($arr[2]);
+            
+            /* Check whether it's valid */
+            if(abs(time()-$passtime)<=3600*24 && $arr[3]===$opath && ($passver==false || ($passver==true && $arr[1]===$passwd)))
+            {
+                if(abs(time()-$passtime)>=3600)
+                {
+                    header('Location: '.encodedir(ROOT_DIR.$opath).'?'.urlencode(encrypt(trim($arr[0]).'|'.($inpassver ? $inpasswd : '').'|'.strval(time()).'|'.$opath,'E',DEF_DOWN)),TRUE,301);
+                    die();
+                }
+                
+                $size=filesize($path);
+                $type=mime_content_type($path);
+                $fname=basename($path);
+                $r_f=0;$r_t=$size-1;
+                
+                /* Support the range header simply */
+                if(isset($_SERVER['HTTP_RANGE']))
+                {
+                    header("{$_SERVER['SERVER_PROTOCOL']} 206 Partial Content");
+                    $range = explode('-',str_replace('=','-',$_SERVER['HTTP_RANGE']));
+                    $r_f=intval(trim($range[1]));
+                    if(intval(trim($range[2]))>0)
+                    $r_t=intval(trim($range[2]));
+                    if($r_t<$r_f || $r_t>=$size)
+                        die;
+                    header("Content-Range: bytes {$r_f}-{$r_t}/{$size}");
+                }
+                header("Content-Length: ".strval(abs($r_t-$r_f)+1));
+                header("Content-Type: {$type}");
+                header("Accept-Ranges: bytes");
+                
+                /* Download or open */
+                if($arr[0]==='dn')
+                {
+                    header("Content-Disposition: attachment; filename=\"{$fname}\"");
+                    header("Conection: Close");
+                }
+                else
+                    header("Conection: Keep-Alive");
+                header('Content-Transfer-Encoding: binary');
+                
+                /* Transfer data */
+                $fp=fopen('php://output','wb');
+                $fpl=fopen($path,'rb');
+                fseek($fpl,$r_f,SEEK_SET);
+                $bytes=0;$size=abs($r_t-$r_f)+1;
+                while($bytes<=$size)
+                {
+                    $nread=READ_BS;
+                    if($nread>abs($size-$bytes)+1)
+                        $nread=abs($size-$bytes)+1;
+                    $buf=fread($fpl,$nread);
+                    if($buf===FALSE)
+                        break;
+                    if(fwrite($fp,$buf)===FALSE)
+                        break;
+                    $bytes+=$nread;
+                }
+                fclose($fpl);
+                fclose($fp);
+                die();
+            }
+        }
+        
+        /* Show the main page */
+        ob_start();
+        htmlmsg();
+        echo '<h1 class="text-center">Accessing to \''.htmlentities(basename($opath)).'\' ......</h1>';
+        
+?>
+<div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="d-table-cell">
+                        <div class="container">
+                            <p class="lead text-center">File path: &nbsp; &nbsp; &nbsp; &nbsp; <?php echo htmlentities($opath); ?></p>
+                            <p class="lead text-center">File size: &nbsp; &nbsp; &nbsp; &nbsp; <?php echo getfilesize($path); ?></p>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+<?php
+        if($passver)
+            checkpassword($inpassver,$inpasswd,$passwd,$opath);
+        
+        /* Have passed the verification, so show the downloading link */
+        if(!$passver || ($passver && !$inpassver))
+        {
+?>
+<div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th style="width:485px;">
+                        <div class="d-table ml-auto"><a class="btn btn-dark" role="button" href="<?php echo encodedir(ROOT_DIR.$opath).'?'.urlencode(encrypt('on'.'|'.($passver ? $inpasswd : '').'|'.strval(time()).'|'.$opath,'E',DEF_DOWN)); ?>" target="_blank">Open</a></div>
+                    </th>
+                    <th style="width:28px;"><strong>or</strong></th>
+                    <th style="width:502px;">
+                        <div class="d-table mr-auto"><a class="btn btn-dark" role="button" href="<?php echo encodedir(ROOT_DIR.$opath).'?'.urlencode(encrypt('dn'.'|'.($passver ? $inpasswd : '').'|'.strval(time()).'|'.$opath,'E',DEF_DOWN)); ?>" target="_blank">Download</a></div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr></tr>
+            </tbody>
+        </table>
+    </div>
+<?php
+        }
+        
+        htmlmsg(false);
+    }
+    else if(is_dir($path))
+    {
+        /* Filter the directory */
+        if(substr($opath,-1)!=='/')
+        {
+            $path.='/';
+            $opath.='/';
+        }
+        if(strpos('.',$path))
+            diemsg('Yep, some unexpected things happened, you know what I mean('.$opath.').');
+        
+        $passwd=getdirpass($opath);
+        if($passwd===FALSE)
+            $passver=false;
+        else
+            $passver=true;
+        
+        /* Show the main page */
+        ob_start();
+        htmlmsg();
+        echo '<h1 class="text-center">Directory of \''.htmlentities($opath).'\' ......</h1>';
+        if($passver)
+            checkpassword($inpassver,$inpasswd,$passwd,$opath);
+?>
+        <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="d-table-cell">
+                        <div class="container">
+                            <p class="lead text-center">Directory path: &nbsp; &nbsp; &nbsp;<?php echo htmlentities($opath); ?></p>
+<?php
+        $endhtml='</div></th></tr></thead><tbody></tbody></table></div>';
+        /* Have passed the verification, so show the elements inside */
+        if(!$passver || ($passver && !$inpassver))
+        {
+            $elecnt=0;
+            $outhtml='<div class="table-responsive"><table class="table"><thead><tr></tr></thead><tbody>';
+            $file=scandir($path);
+            foreach($file as $val)
+            {
+                /* Filter the filename */
+                if($val==='.' || $val==='..' || invalidfilename($val))
+                    continue;
+                $elecnt++;
+                $fopath=$opath.$val;
+                $fpath=dirname(__FILE__).FILE_DIR.$fopath;
+                $ispd=is_dir($fpath);
+                
+                /* Construct the html code */
+                $outhtml.='<tr><td><p class="text-center" style="width:700px;">'.htmlentities($ispd ? $val.'/' : $val).'</p></td><td style="width:170px;">'.($ispd ? htmlentities('<DIR>') : getfilesize($fpath)).'</td><td style="width:167px;"><a class="btn btn-dark" role="button" href="'.encodedir($fopath).'" target="_blank" style="width:64px;">Open</a></td></tr>';
+            }
+            $endhtml='<p class="lead text-center">Total elements: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; '.strval($elecnt).'</p>'.$endhtml;
+            $outhtml.='</tbody></table></div>';
+        }
+        echo $endhtml.$outhtml;
+        htmlmsg(false);
+    }
+    else
+        diemsg('The file/directory doesn\'t exist!');
+    
+    ob_flush();
+?>
